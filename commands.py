b7095869a6fadc6b661e9d0c29353669027ece23
Kacper Sieradzinski <ksieradzinski@gmail.com>
2020-10-02 20:15:55 +0200

    initial commit

---
+++ commands.py
@@ -0,0 +1,46 @@
+import click
+from database import Database
+from os import getenv
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def setup():
+    print('Tworze tabele w bazie danych')
+    db = Database(getenv('DB_NAME'))
+    db.create_table('CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, url TEXT)')
+
+
+@click.command()
+@click.argument('category')
+@click.argument('url')
+def add(category: str, url: str):
+    print('Dodaję nowy adres url')
+    db = Database(getenv('DB_NAME'))
+    db.insert ('urls', None, category, url)
+
+
+@click.command(name='categories')
+def fetch_categories():
+    print('Lista kategorii:')
+    db = Database(getenv('DB_NAME'))
+    categories = db.fetch_distinct('urls', 'category')
+
+    for name in categories:
+        print(name[0])
+
+
+@click.command()
+@click.argument('category')
+def index(category: str):
+    print(f'Lista linków z kategorii {category}:')
+    db = Database(getenv('DB_NAME'))
+    links = db.fetch_all('urls', category=category)
+
+    for link in links:
+        print(link[2])
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from commands import cli, setup, add, fetch_categories, index
+from dotenv import load_dotenv
+load_dotenv()
+
+cli.add_command(setup)
+cli.add_command(add)
+cli.add_command(fetch_categories)
+cli.add_command(index)
+
+if __name__ == '__main__':
+    cli()
+
